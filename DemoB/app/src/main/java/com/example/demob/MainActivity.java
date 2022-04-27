09eaceabeb9c774d214ab416729470aeaf1ca350
zhangsen <zhangsen839705693@163.com>
2022-04-27 19:37:42 +0800

    1

--- DemoB/app/src/main/java/com/example/demob/MainActivity.java
+++ DemoB/app/src/main/java/com/example/demob/MainActivity.java
@@ -15,9 +15,6 @@ import com.watchdog.ipc.entry.Message;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "MainActivity";
-    private Button bottonConnect;
-    private Button bottonDisConnect;
-    private Button bottonIsConnect;
     private Button btnSendMessage;
     private Button btnRegisterListener;
     private Button btnUnRegisterListener;
@@ -28,17 +25,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottonConnect = findViewById(R.id.btn_connect);
-        bottonDisConnect = findViewById(R.id.btn_disconnect);
-        bottonIsConnect = findViewById(R.id.btn_isconnect);
+
         btnSendMessage = findViewById(R.id.btn_send_message);
         btnRegisterListener = findViewById(R.id.btn_register_listener);
         btnUnRegisterListener = findViewById(R.id.btn_unregister_listener);
         btn_buy_apple = findViewById(R.id.btn_buy_apple);
 
-        bottonConnect.setOnClickListener(this);
-        bottonDisConnect.setOnClickListener(this);
-        bottonIsConnect.setOnClickListener(this);
+
         btnSendMessage.setOnClickListener(this);
         btnRegisterListener.setOnClickListener(this);
         btnUnRegisterListener.setOnClickListener(this);
@@ -63,15 +56,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.btn_connect:
-
-                break;
-            case R.id.btn_disconnect:
-
-                break;
-            case R.id.btn_isconnect:
-
-                break;
             case R.id.btn_send_message:
                 Message message = new Message();
                 message.setContent("message send from demoB");
