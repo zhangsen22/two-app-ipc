b5d0f25079d181a342835566c9f1d08ed366912e
zhangsen <zhangsen839705693@163.com>
2022-05-27 11:30:05 +0800

    1

--- DemoA/keytool-importkeypair-master/signature.sh
+++ DemoA/keytool-importkeypair-master/signature.sh
@@ -2,7 +2,8 @@
 
 # 转换平台签名命令
 # 这个工具的作用是将系统签名的相关信息导入到已有的签名文件里。
-./keytool-importkeypair -k ../signature/demo.jks -p 123456 -pk8 platform.pk8 -cert platform.x509.pem -alias demo
+./keytool-importkeypair -k ../signature/demo.jks -p 123456 -pk8 7/platform.pk8 -cert 7/platform.x509.pem -alias demo
+#./keytool-importkeypair -k ../signature/demo.jks -p 123456 -pk8 12/platform.pk8 -cert 12/platform.x509.pem -alias demo
 #keytool-importkeypair -k ~/.android/debug.keystore -p android -pk8 platform.pk8 -cert platform.x509.pem -alias platform
 # demo.jks : 签名文件
 
